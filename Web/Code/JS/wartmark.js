68f1f23467724c982fef89a0ee03afd2b7bcf9d1
Dev Hjz <h010zy@163.com>
2021-10-30 19:49:23 +0800

    Add files via upload

--- Web/Code/JS/wartmark.js
+++ Web/Code/JS/wartmark.js
@@ -1,83 +1,75 @@
-function watermark(settings) {
+function watermark(t1,t2,t3){
+    var space = 1;
+    var columnExtra = 0;
+    var maxWidth = document.documentElement.clientWidth;
+    var maxHeight = document.documentElement.clientHeight;
+    var intervalWidth = 320 * space;    //间隔宽度
+    var intervalheight = 160 * space;   //间隔高度
+    var rowNumber = (maxWidth - 40 -200) / intervalWidth + columnExtra;    //横向个数
+    var coumnNumber = (maxHeight - 40-80) / intervalheight;   //纵向个数
+    
     //默认设置
     var defaultSettings = {
-        watermark_txt: "text",
-        watermark_x: 20, //水印起始位置x轴坐标
-        watermark_y: 20, //水印起始位置Y轴坐标
-        watermark_rows: 20, //水印行数
-        watermark_cols: 20, //水印列数
-        watermark_x_space: 100, //水印x轴间隔
-        watermark_y_space: 50, //水印y轴间隔
         watermark_color: '#aaa', //水印字体颜色
-        watermark_alpha: 0.4, //水印透明度
-        watermark_fontsize: '15px', //水印字体大小
-        watermark_font: '微软雅黑', //水印字体
-        watermark_width: 210, //水印宽度
+        watermark_alpha: 0.3, //水印透明度
+        watermark_fontsize: '16px', //水印字体大小
+        watermark_font: '宋体', //水印字体
+        watermark_width: 200, //水印宽度
         watermark_height: 80, //水印长度
-        watermark_angle: 20 //水印倾斜度数
+        watermark_angle: 15, //水印倾斜度数
+        watermark_offset: 100 //水印offset
     };
-    if (arguments.length === 1 && typeof arguments[0] === "object") {
-        var src = arguments[0] || {};
-        for (key in src) {
-            if (src[key] && defaultSettings[key] && src[key] === defaultSettings[key]) continue;
-            else if (src[key]) defaultSettings[key] = src[key];
+        
+    var _temp = document.createDocumentFragment();
+    var _waterDiv = document.createElement('div');
+  //   _waterDiv.setAttribute("style","width: 100%; height: 100%; overflow: hidden; position: relative;") ;
+  //   _waterDiv.style.cssText = "width: 100%; height: 100%; overflow: hidden; position: relative;";
+    for(var i = 0; i < rowNumber; i++){
+        for(var j = 0; j < coumnNumber; j++){
+            var x = intervalWidth*i + 20;
+            var y = intervalheight*j + 30;
+            var mark_div = document.createElement('div');
+            mark_div.id = 'mark_div' + i + j;
+            mark_div.className = 'mark_div';
+            ///三个节点
+            var span0 = document.createElement('div');
+            span0.appendChild(document.createTextNode(t1));
+            var span1 = document.createElement('div');
+            span1.appendChild(document.createTextNode(t2));
+            var span2 = document.createElement('div');
+            span2.appendChild(document.createTextNode(t3));
+            mark_div.appendChild(span0);
+            mark_div.appendChild(span1);
+            mark_div.appendChild(span2);
+            //设置水印div倾斜显示
+            mark_div.style.webkitTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+            mark_div.style.MozTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+            mark_div.style.msTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+            mark_div.style.OTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+            mark_div.style.transform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
+            mark_div.style.visibility = "";
+            mark_div.style.position = "absolute";
+            mark_div.style.left = defaultSettings.watermark_offset + x + 'px';
+            mark_div.style.top = y + 'px';
+            mark_div.style.overflow = "hidden";
+            mark_div.style.zIndex = "9999";
+            mark_div.style.pointerEvents = 'none'; //pointer-events:none  让水印不阻止交互事件
+            mark_div.style.opacity = defaultSettings.watermark_alpha;
+            mark_div.style.fontSize = defaultSettings.watermark_fontsize;
+            mark_div.style.fontFamily = defaultSettings.watermark_font;
+            mark_div.style.color = defaultSettings.watermark_color;
+            mark_div.style.textAlign = "center";
+            mark_div.style.width = defaultSettings.watermark_width + 'px';
+            mark_div.style.height = defaultSettings.watermark_height + 'px';
+            mark_div.style.display = "block";
+            
+            _waterDiv.appendChild(mark_div);
         }
     }
-    var oTemp = document.createDocumentFragment();
-    //获取页面最大宽度
-    var page_width = Math.max(document.body.scrollWidth, document.body.clientWidth);
-    var cutWidth = page_width * 0.0150;
-    var page_width = page_width - cutWidth;
-    //获取页面最大高度
-    var page_height = Math.max(document.body.scrollHeight, document.body.clientHeight) + 450;
-    page_height = Math.max(page_height, window.innerHeight - 30);
-    //如果将水印列数设置为0，或水印列数设置过大，超过页面最大宽度，则重新计算水印列数和水印x轴间隔
-    if (defaultSettings.watermark_cols == 0 || (parseInt(defaultSettings.watermark_x + defaultSettings.watermark_width * defaultSettings.watermark_cols + defaultSettings.watermark_x_space * (defaultSettings.watermark_cols - 1)) > page_width)) {
-        defaultSettings.watermark_cols = parseInt((page_width - defaultSettings.watermark_x + defaultSettings.watermark_x_space) / (defaultSettings.watermark_width + defaultSettings.watermark_x_space));
-        defaultSettings.watermark_x_space = parseInt((page_width - defaultSettings.watermark_x - defaultSettings.watermark_width * defaultSettings.watermark_cols) / (defaultSettings.watermark_cols - 1));
-    }
-    //如果将水印行数设置为0，或水印行数设置过大，超过页面最大长度，则重新计算水印行数和水印y轴间隔
-    if (defaultSettings.watermark_rows == 0 || (parseInt(defaultSettings.watermark_y + defaultSettings.watermark_height * defaultSettings.watermark_rows + defaultSettings.watermark_y_space * (defaultSettings.watermark_rows - 1)) > page_height)) {
-        defaultSettings.watermark_rows = parseInt((defaultSettings.watermark_y_space + page_height - defaultSettings.watermark_y) / (defaultSettings.watermark_height + defaultSettings.watermark_y_space));
-        defaultSettings.watermark_y_space = parseInt(((page_height - defaultSettings.watermark_y) - defaultSettings.watermark_height * defaultSettings.watermark_rows) / (defaultSettings.watermark_rows - 1));
-    }
-    var x;
-    var y;
-    for (var i = 0; i < defaultSettings.watermark_rows; i++) {
-        y = defaultSettings.watermark_y + (defaultSettings.watermark_y_space + defaultSettings.watermark_height) * i;
-        for (var j = 0; j < defaultSettings.watermark_cols; j++) {
-            x = defaultSettings.watermark_x + (defaultSettings.watermark_width + defaultSettings.watermark_x_space) * j;
-            var mask_div = document.createElement('div');
-            mask_div.id = 'mask_div' + i + j;
-            mask_div.className = 'mask_div';
-            mask_div.appendChild(document.createTextNode(defaultSettings.watermark_txt));
-            //设置水印div倾斜显示
-            mask_div.style.webkitTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-            mask_div.style.MozTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-            mask_div.style.msTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-            mask_div.style.OTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-            mask_div.style.transform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
-            mask_div.style.visibility = "";
-            mask_div.style.position = "absolute";
-            mask_div.style.left = x + 'px';
-            mask_div.style.top = y + 'px';
-            mask_div.style.overflow = "hidden";
-            mask_div.style.zIndex = "9999";
-            //让水印不遮挡页面的点击事件
-            mask_div.style.pointerEvents = 'none';
-            mask_div.style.opacity = defaultSettings.watermark_alpha;
-            mask_div.style.fontSize = defaultSettings.watermark_fontsize;
-            mask_div.style.fontFamily = defaultSettings.watermark_font;
-            mask_div.style.color = defaultSettings.watermark_color;
-            mask_div.style.textAlign = "center";
-            mask_div.style.width = defaultSettings.watermark_width + 'px';
-            mask_div.style.height = defaultSettings.watermark_height + 'px';
-            mask_div.style.display = "block";
-            oTemp.appendChild(mask_div);
-        };
-    };
-    document.body.appendChild(oTemp);
-}
+    _temp.appendChild(_waterDiv);
+    
+    document.body.appendChild(_temp);
+  }
 
 function getNow() {
     var d = new Date();
@@ -95,6 +87,6 @@ function getNow() {
             return t;
         }
     }
-    var time = year + '年' + month + '月' + day + '日 ' + hour + '时' + minute + '分' + second + '秒';
+    var time = year + '-' + month + '-' + day;
     return time;
 }
